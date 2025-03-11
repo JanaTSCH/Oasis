602aaf57e1678885888782702945f417e219596c
Jana-script <jana.jscript@gmail.com>
2025-03-11 20:51:30 +0100

    global styles with styled components

--- the-wild-oasis/src/App.jsx
+++ the-wild-oasis/src/App.jsx
@@ -1,5 +1,46 @@
+import styled from "styled-components";
+import GlobalStyles from "./styles/GlobalStyles";
+
+const H1 = styled.h1`
+  font-size: 30px;
+  font-weight: 600;
+  background-color: #fcd5d5;
+`;
+
+const Button = styled.button`
+  font-size: 1.4rem;
+  padding: 1.2rem 1.6rem;
+  font-weight: 500;
+  border: none;
+  border-radius: 7px;
+  background-color: #a87272;
+  color: white;
+  margin: 20px;
+  cursor: pointer;
+`;
+
+const Input = styled.input`
+  border: 1px solid #ddd;
+  border-radius: 5px;
+  padding: 0.8rem 1.2rem;
+`;
+
+const StyledApp = styled.div`
+  background-color: #c9a3a3;
+  padding: 20px;
+`;
 function App() {
-  return <div>HW</div>;
+  return (
+    <>
+      <GlobalStyles />
+      <StyledApp>
+        <H1>The Wild Oasis</H1>
+        <Button onClick={() => alert("Check in")}>Check in</Button>
+        <Button onClick={() => alert("Check out")}>Check in</Button>
+        <Input type="number" placeholder="Number of guests"></Input>
+      </StyledApp>
+    </>
+  );
 }
 
 export default App;
