3f44db20ed79e51585f254270bc2d1cab823e740
Jana-script <jana.jscript@gmail.com>
2025-03-15 20:00:06 +0100

    uploading images to supabase

--- the-wild-oasis/src/services/apiCabins.js
+++ the-wild-oasis/src/services/apiCabins.js
@@ -1,4 +1,4 @@
-import supabase from "./supabase";
+import supabase, { supabaseUrl } from "./supabase";
 
 export async function getCabins() {
   const { data, error } = await supabase.from("cabins").select("*");
@@ -10,11 +10,38 @@ export async function getCabins() {
   return data;
 }
 
+//https://jvtrfehrjelxsmgeujlj.supabase.co/storage/v1/object/public/cabin-images//cabin-001.jpg
+
 export async function createCabin(newCabin) {
-  const { data, error } = await supabase.from("cabins").insert(newCabin);
+  const imageName = `${Math.random()}-${newCabin.image.name}`.replaceAll(
+    "/",
+    ""
+  );
+
+  const imagePath = `${supabaseUrl}/storage/v1/object/public/cabin-images//${imageName}`;
+
+  // 1. Create cabin
+  const { data, error } = await supabase
+    .from("cabins")
+    .insert({ ...newCabin, image: imagePath });
   if (error) {
     console.error(error);
     throw new Error("Cabins could not be created");
+  }
+
+  // 2. Upload the image
+  const { error: storageError } = await supabase.storage
+    .from("cabin-images")
+    .upload(imageName, newCabin.image);
+
+  // 3. Delete the cabin if image upload fails
+
+  if (storageError) {
+    await supabase.from("cabins").delete().eq("id", data.id);
+    console.error(storageError);
+    throw new Error(
+      "Cabin image couldn't be uploaded and the cabin wasn't created"
+    );
   }
 
   return data;
