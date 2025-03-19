552d5c0fdd7e5635d52aebf9b5f96d8448729634
Jana-script <jana.jscript@gmail.com>
2025-03-19 19:49:13 +0100

    Compound Component
    Converting the Modal to a Compound Component

--- the-wild-oasis/src/ui/Modal-v1.jsx
+++ the-wild-oasis/src/ui/Modal-v1.jsx
@@ -1,5 +1,3 @@
-import { cloneElement, useContext } from "react";
-import { createContext, useState } from "react";
 import { createPortal } from "react-dom";
 import { HiXMark } from "react-icons/hi2";
 import styled from "styled-components";
@@ -53,47 +51,20 @@ const Button = styled.button`
   }
 `;
 
-const ModalContext = createContext();
-
 // eslint-disable-next-line react/prop-types
-function Modal({ children }) {
-  const [openName, setOpenName] = useState("");
-  const close = () => setOpenName("");
-  const open = setOpenName;
-
-  return (
-    <ModalContext.Provider value={{ openName, close, open }}>
-      {children}
-    </ModalContext.Provider>
-  );
-}
-
-function Open({ children, opens: opensWindowName }) {
-  const { open } = useContext(ModalContext);
-
-  return cloneElement(children, { onClick: () => open(opensWindowName) });
-}
-
-// eslint-disable-next-line react/prop-types
-function Window({ children, name }) {
-  const { openName, close } = useContext(ModalContext);
-  if (name !== openName) return null;
-
+function Modal({ children, onClose }) {
   return createPortal(
     <Overlay>
       <StyledModal>
-        <Button onClick={close}>
+        <Button onClick={onClose}>
           <HiXMark />
         </Button>
-        <div>{cloneElement(children, { onCloseModal: close })}</div>
+        <div>{children}</div>
       </StyledModal>
     </Overlay>,
     document.body
     // document.querySelector()
   );
 }
-
-Modal.Open = Open;
-Modal.Window = Window;
 
 export default Modal;
