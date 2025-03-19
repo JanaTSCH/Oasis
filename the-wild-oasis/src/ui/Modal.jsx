db9db39d560d31f892d4db42e58e3e5f47b83e6b
Jana-script <jana.jscript@gmail.com>
2025-03-19 22:04:35 +0100

    click outside and close

--- the-wild-oasis/src/ui/Modal.jsx
+++ the-wild-oasis/src/ui/Modal.jsx
@@ -1,8 +1,9 @@
-import { cloneElement, useContext } from "react";
+import { cloneElement, useContext, useEffect, useRef } from "react";
 import { createContext, useState } from "react";
 import { createPortal } from "react-dom";
 import { HiXMark } from "react-icons/hi2";
 import styled from "styled-components";
+import { useOutsideClick } from "../hooks/useOutsideClick";
 
 const StyledModal = styled.div`
   position: fixed;
@@ -77,11 +78,13 @@ function Open({ children, opens: opensWindowName }) {
 // eslint-disable-next-line react/prop-types
 function Window({ children, name }) {
   const { openName, close } = useContext(ModalContext);
+  const ref = useOutsideClick(close);
+
   if (name !== openName) return null;
 
   return createPortal(
     <Overlay>
-      <StyledModal>
+      <StyledModal ref={ref}>
         <Button onClick={close}>
           <HiXMark />
         </Button>
