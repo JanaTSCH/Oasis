e7f734e0e1629aaf555b7627c14f79f8a77d4399
Jana-script <jana.jscript@gmail.com>
2025-03-20 12:33:04 +0100

    klein fix

--- the-wild-oasis/src/features/cabins/CabinRow.jsx
+++ the-wild-oasis/src/features/cabins/CabinRow.jsx
@@ -10,6 +10,7 @@ import Modal from "../../ui/Modal";
 import ConfirmDelete from "../../ui/ConfirmDelete";
 import Table from "../../ui/Table";
 import Menus from "../../ui/Menus";
+import { deleteCabin } from "../../services/apiCabins";
 
 // const TableRow = styled.div`
 //   display: grid;
@@ -85,36 +86,36 @@ function CabinRow({ cabin }) {
         <span>&mdash;</span>
       )}
       <div>
-        {/* <button disabled={isCreating} onClick={handleDuplicate}>
-          <HiSquare2Stack />
-        </button> */}
         <Modal>
           <Menus.Menu>
             <Menus.Toggle id={cabinId} />
+
             <Menus.List id={cabinId}>
               <Menus.Button icon={<HiSquare2Stack />} onClick={handleDuplicate}>
                 Duplicate
               </Menus.Button>
+
               <Modal.Open opens="edit">
                 <Menus.Button icon={<HiPencil />}>Edit</Menus.Button>
               </Modal.Open>
+
+              <Modal.Open opens="delete">
+                <Menus.Button icon={<HiTrash />}>Delete</Menus.Button>
+              </Modal.Open>
             </Menus.List>
+
+            <Modal.Window name="edit">
+              <CreateCabinForm cabinToEdit={cabin} />
+            </Modal.Window>
+
+            <Modal.Window name="delete">
+              <ConfirmDelete
+                resourceName="cabins"
+                disabled={isDeleting}
+                onConfirm={() => deleteCabin(cabinId)}
+              />
+            </Modal.Window>
           </Menus.Menu>
-
-          <Modal.Window name="edit">
-            <CreateCabinForm cabinToEdit={cabin} />
-          </Modal.Window>
-
-          <Modal.Open opens="delete">
-            <Menus.Button icon={<HiTrash />}>Delete</Menus.Button>
-          </Modal.Open>
-          <Modal.Window name="delete">
-            <ConfirmDelete
-              resourceName="cabins"
-              disabled={isDeleting}
-              onConfirm={() => deleteCabinApi(cabinId)}
-            />
-          </Modal.Window>
         </Modal>
       </div>
     </Table.Row>
