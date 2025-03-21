d9faba47c5a7c859d261449d212b8319e1f76eb7
Jana-script <jana.jscript@gmail.com>
2025-03-21 19:18:24 +0100

    API-side sorting: sorting bookings + filtering

--- the-wild-oasis/src/features/bookings/useBookings.js
+++ the-wild-oasis/src/features/bookings/useBookings.js
@@ -13,13 +13,19 @@ export function useBookings() {
       : { field: "status", value: filterValue };
   // : { field: "totalPrice", value: 5000, method: "qte" };
 
+  //SORT
+  const sortByRaw = searchParams.get("sortBy") || "startDate-asc";
+  const [field, direction] = sortByRaw.split("-");
+  const sortBy = { field, direction };
+  // const modifier = direction === "asc" ? 1 : -1;
+
   const {
     isLoading,
     data: bookings,
     error,
   } = useQuery({
-    queryKey: ["bookings", filter],
-    queryFn: () => getBookings({ filter }),
+    queryKey: ["bookings", filter, sortBy],
+    queryFn: () => getBookings({ filter, sortBy }),
   });
   return { bookings, isLoading, error };
 }
