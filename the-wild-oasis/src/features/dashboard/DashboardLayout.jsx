eb82c1f5f536f0680675888d316b10a9089e3eac
Jana-script <jana.jscript@gmail.com>
2025-04-21 17:18:56 +0200

    computing recent bookings and stays

--- the-wild-oasis/src/features/dashboard/DashboardLayout.jsx
+++ the-wild-oasis/src/features/dashboard/DashboardLayout.jsx
@@ -1,4 +1,7 @@
 import styled from "styled-components";
+import { useRecentBookings } from "./useRecentBookings";
+import Spinner from "../../ui/Spinner";
+import { useRecentStays } from "./useRecentStays";
 
 const StyledDashboardLayout = styled.div`
   display: grid;
@@ -8,6 +11,10 @@ const StyledDashboardLayout = styled.div`
 `;
 
 function DashboardLayout() {
+  const { bookings, isLoading: isBooking } = useRecentBookings();
+  const { stays, confirmedStays, isLoading: staysLoading } = useRecentStays();
+
+  if (isBooking || staysLoading) return <Spinner />;
   return (
     <StyledDashboardLayout>
       <div>Statistics</div>
