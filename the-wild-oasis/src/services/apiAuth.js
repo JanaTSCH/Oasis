dec46cc371f339f38b4bd143fecb23e1b0c23825
Jana-script <jana.jscript@gmail.com>
2025-04-22 23:57:49 +0200

    fix 12

--- the-wild-oasis/src/services/apiAuth.js
+++ the-wild-oasis/src/services/apiAuth.js
@@ -11,7 +11,8 @@ export async function signup({ fullName, email, password }) {
       },
     },
   });
-  if (error) throw Error(error.message);
+
+  if (error) throw new Error(error.message);
 
   return data;
 }
@@ -22,7 +23,7 @@ export async function login({ email, password }) {
     password,
   });
 
-  if (error) throw Error(error.message);
+  if (error) throw new Error(error.message);
 
   return data;
 }
@@ -34,43 +35,41 @@ export async function getCurrentUser() {
   const { data, error } = await supabase.auth.getUser();
 
   if (error) throw new Error(error.message);
-
   return data?.user;
 }
 
 export async function logout() {
   const { error } = await supabase.auth.signOut();
-
   if (error) throw new Error(error.message);
 }
 
 export async function updateCurrentUser({ password, fullName, avatar }) {
   // 1. Update password OR fullName
-
   let updateData;
   if (password) updateData = { password };
   if (fullName) updateData = { data: { fullName } };
+
   const { data, error } = await supabase.auth.updateUser(updateData);
 
   if (error) throw new Error(error.message);
   if (!avatar) return data;
 
-  // 2. Update avatar image
+  // 2. Upload the avatar image
   const fileName = `avatar-${data.user.id}-${Math.random()}`;
 
   const { error: storageError } = await supabase.storage
     .from("avatars")
     .upload(fileName, avatar);
+
   if (storageError) throw new Error(storageError.message);
 
   // 3. Update avatar in the user
-
-  const { data: updatedUser, error: dbError } = await supabase.auth.updateUser({
+  const { data: updatedUser, error: error2 } = await supabase.auth.updateUser({
     data: {
       avatar: `${supabaseUrl}/storage/v1/object/public/avatars/${fileName}`,
     },
   });
-  if (dbError) throw new Error(dbError.message);
 
+  if (error2) throw new Error(error2.message);
   return updatedUser;
 }
