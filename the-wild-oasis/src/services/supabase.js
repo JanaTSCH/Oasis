071f331c1744288e4580f05c5069b8b62a4056b7
Jana-script <jana.jscript@gmail.com>
2025-04-22 23:28:57 +0200

    Revert "fix 4"
    This reverts commit 23c0e022fb272abe519723e0d1ff51b5d9fb7efa.

--- the-wild-oasis/src/services/supabase.js
+++ the-wild-oasis/src/services/supabase.js
@@ -1,8 +1,7 @@
-/* eslint-disable no-undef */
 import { createClient } from "@supabase/supabase-js";
 
-export const supabaseUrl = process.env.SUPABASE_URL;
-const supabaseKey = process.env.SUPABASE_KEY;
+export const supabaseUrl = SUPABASE_URL;
+const supabaseKey = SUPABASE_KEY;
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
