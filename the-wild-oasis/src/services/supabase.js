5554de268d0f71f733498ba6854bc46d4130868b
Jana-script <jana.jscript@gmail.com>
2025-04-26 14:15:07 +0200

    fix 19

--- the-wild-oasis/src/services/supabase.js
+++ the-wild-oasis/src/services/supabase.js
@@ -1,8 +1,8 @@
 /* eslint-disable no-undef */
 import { createClient } from "@supabase/supabase-js";
 
-export const supabaseUrl = process.env.REACT_APP_SUPABASE_URL;
+const supabaseUrl = process.env.REACT_APP_SUPABASE_URL;
 const supabaseKey = process.env.REACT_APP_SUPABASE_KEY;
 const supabase = createClient(supabaseUrl, supabaseKey);
 
-export default supabase;
+export default { supabase, supabaseUrl };
